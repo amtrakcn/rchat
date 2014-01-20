e5e55fe2b9368c87bda9930fea48c03bab75a2e5
Changmin Shen <amtrakcn@gmail.com>
2014-01-19 21:28:41 -0800

    socket connection

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
-chat_server = Server.new
+require 'socket'
+
+chat_server = TCPServer.new 2000
 loop do
   #connect puts (chat_server.excute(connection.gets))
+
+  Thread.start(server.accept) do |client|
+  end
 end
